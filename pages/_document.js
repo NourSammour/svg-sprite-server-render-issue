bcec0686cd710ee5a9f244489148f207eb4074dd
NourSammour <nour.sammour@hotmail.com>
2020-06-10 16:03:43 +0300

    init

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import sprite from 'svg-sprite-loader/runtime/sprite.build';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const spriteContent = sprite.stringify();
+
+    return {
+      spriteContent,
+      ...initialProps,
+    };
+  }
+
+  render() {
+    return (
+        <html>
+        <Head>{/* your head if needed */}</Head>
+        <body>
+        <div dangerouslySetInnerHTML={{ __html: this.props.spriteContent }} />
+        <Main />
+        <NextScript />
+        </body>
+        </html>
+    );
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MyIcon from './icons/vercel.svg?sprite';
+
+export default () => (
+    <div>
+      my page..
+      <MyIcon />
+    </div>
+);
